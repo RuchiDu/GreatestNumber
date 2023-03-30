dd88e93210a8c71d4393beb93dafd87818d61d61
RuchiDu <ruchi.dukhande97@gmail.com>
2023-03-30 17:36:55 +0530

    initial

---
+++ Greatest number.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter your number 1 : "))
+num2  = int(input("Enter your number 2 : "))
+num3 =int (input("Enter your number 3 :"))
+num4  = int(input("Enter your number 4 :"))
+if(num1 > num4):
+    f1=num1
+    pass
+else:
+    f1=num4
+if(num2>num3):
+    f2=num2
+    pass
+else:
+    f2=num3
+if(f1>f2):
+    print(f1)
+    pass
+else:
+    print(f2)
